91b902850cd2552d944f1b85d6c6047fcb889f55
ISTKHAROP <istkharalam2006@gmail.com>
2025-08-08 23:16:37 +0530

    Update gadd.py

--- SONALI_MUSIC/plugins/tools/gadd.py
+++ SONALI_MUSIC/plugins/tools/gadd.py
@@ -2,7 +2,7 @@ import asyncio
 
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-OWNERS = "7995626429"
+OWNERS = "7633166578"
 from SONALI_MUSIC import app
 from SONALI_MUSIC.utils.database import add_served_chat, get_assistant
 
