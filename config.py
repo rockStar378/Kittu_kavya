00a936b7918fabd4b1e5a82afef596161cbcccd5
rockStar378 <shahzadluqman092@gmail.com>
2025-11-22 08:17:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-API_KEY = getenv("API_KEY", 'NxGBNexGenBotse5a838') # youtube song api ke
+API_KEY = getenv("API_KEY", 'NxGBNexGenBotsb1880e') # youtube song api ke
 
 PRIVACY_LINK = getenv("PRIVACY_LINK", "https://telegra.ph/Privacy-Policy-for-YukkiMusic-08-30")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/shree_update")
